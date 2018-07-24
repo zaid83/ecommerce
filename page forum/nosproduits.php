9008497a78f0dce75a978322e2ad3c5f44703c54
greg13170 <39373118+greg13170@users.noreply.github.com>
2018-07-24 15:32:20 +0200

    Add files via upload

---
+++ page forum/nosproduits.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="">
+    <link rel="stylesheet" type="text/css" href="nosproduits.css">
+    <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Noticia+Text" rel="stylesheet">
+
+
+</head>
+
+
+<body>
+
+
+
+    <div class="navbaraccueil">
+
+        <a class="active" href="#">CONTACT</a>
+        <a href="#">SERVICES</a>
+        <a href="#">PRODUITS</a>
+        <a href="#">ACCUEIL</a>
+
+    </div>
+
+
+    <div class="texthaut ">
+
+        <strong> <h3>Nos produits :</h3>
+        <b>En provenance de l'Europe toute entière<br>
+et regroupant des produits locaux,<br>
+nos fromages sont de qualité<br> et
+vous feront voyager dans 
+les pays du continent
+	</b>
+           </strong>
+    </div>
+    <img class="fromage" src="images/fromage2.jpg" alt="photodefromage tonton!">
+
+    <div class="textbas">
+       <strong> <h4>Nos types de fromages :</h4>
+        <p>Nous disposons de tout type de produit,<br> en partant du fromage de brebis au<br> fromage de chèvres ainsi que du <br> fromage au lait de vache.</p>
+           </strong>
+    </div>
+
+
+
+
+
+    <img class="logo" src="images/logo.png" alt="logo simplon">
+
+</body>
+
+</html>
